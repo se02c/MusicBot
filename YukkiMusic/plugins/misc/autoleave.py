92ffd1c7aaefab1dba58d87dca41e5ed06782bf0
se02c <110836080+se02c@users.noreply.github.com>
2022-08-12 08:56:33 +0200

    Update autoleave.py

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -79,7 +79,7 @@ async def auto_end():
                 try:
                     await app.send_message(
                         chat_id,
-                        "Bot has left voice chat due to inactivity to avoid overload on servers. No-one was listening to the bot on voice chat.",
+                        "#ZERO\n◍ خرج المساعد من المحادثه الصوتيه\nوذلك لعدم وجود شخص في المحادثه\nوهذا اهدار لسيرفر √",
                     )
                 except:
                     continue
